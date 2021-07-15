d690e1721c22ed9ec58056d324355211eb916444
geethikareddyk9 <85934185+geethikareddyk9@users.noreply.github.com>
2021-07-15 16:17:46 +0530

    Add files via upload

---
+++ Arithemetic operators/nomuloperator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,res;
+	scanf("%i%i",&a,&b);
+	res = a*b;
+	printf("%i",res);
+	return 0;
+}
---
+++ Arithemetic operators/notlastdigits.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int n,notltdt;
+	scanf("%i",&n);
+	notltdt = n/100 ;
+	printf("%i",notltdt);
+	return 0;
+}
