348c08e9619f1a45b5bf1ac50b2a325f9165ace5
geethikareddyk9 <85934185+geethikareddyk9@users.noreply.github.com>
2021-07-15 16:33:02 +0530

    Add files via upload

---
+++ Arithemetic operators/cube_of_num.c
@@ -0,0 +1,22 @@
+/*.  Program to find cube of a number 
+without using built-in function 
+and with using built-in function*/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int num,cube,cube2;
+	printf("Enter a number:");
+	scanf("%i",&num);
+	
+	// without built-in function
+	cube = num*num*num ;
+	printf("cube of given number is %i",cube);
+	
+	//using built-in function
+	cube2 = pow(num,3) ;
+	printf("\ncube of given number is %i",cube2);
+}
+
+
