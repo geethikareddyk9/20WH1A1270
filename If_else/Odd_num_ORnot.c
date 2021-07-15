5764928c173290d994483930be5dec9222d63fea
geethikareddyk9 <85934185+geethikareddyk9@users.noreply.github.com>
2021-07-15 21:14:31 +0530

    Add files via upload

---
+++ If_else/Odd_num_ORnot.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int n ;
+	scanf("%i",&n);
+	
+	if ( n%2!= 0)
+	printf("The given number is ODD. ");
+	else
+	printf("The given number is not ODD. ");
+	return 0;
+	// hurray!!
+	//great work :P 
+}
---
+++ If_else/positive_num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%i",&n);
+	if (n>=0)
+	printf("The given number is positive. ");
+	else 
+	printf("The given number is negative. ");
+	return 0;
+	
+}
