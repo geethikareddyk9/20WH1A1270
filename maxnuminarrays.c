34d476686624fcad5fe6cb98dfef8ee8396355d6
geethikareddyk9 <85934185+geethikareddyk9@users.noreply.github.com>
2021-07-16 15:24:25 +0530

    Add files via upload

---
+++ maxnuminarrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int i,A[3]={9,5,4},max,loc=0;
+	for (i=1;i<3;i++)
+		/*if (A[i]>A[i+1])
+			max=A[i];
+		elif(A[i+1]>A[i])
+			max=A[i+1];
+		else*/
+		if (A[i]<A[loc])
+			loc=i;
+			
+		printf("%d %d",A[loc]);
+		
+	for(i=0;i<)
+		return 0;
+	
+	// max,min,avg
+}
